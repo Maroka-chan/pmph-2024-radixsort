6d0181f172c0b33a23336cc6f25fd739eeda62ec
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-10-09 14:30:58 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ { flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ futhark gcc ];
+        };
+      };
+
+      flake = {
+
+      };
+    };
+}
---
+++ radix.fut
@@ -0,0 +1,3 @@
+import "lib/github.com/diku-dk/sorts/radix_sort"
+
+def main = radix_sort_int i32.num_bits i32.get_bit [5,7,-1,2,-2]

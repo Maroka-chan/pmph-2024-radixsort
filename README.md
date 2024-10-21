12cf0a4885474d500d0b26924d1d9cfa58019ea1
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2024-10-21 16:20:33 +0200

    add cleanup after benchmarking

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Futhark benchmark
+
 ```bash
-futhark dataset --i32-bounds=-9999:9999 -g "[1000000]i32" > data.in
-futhark c radix.fut && cat data.in | ./radix > data.out
-futhark bench
+./bench.sh
 ```
--- bench.sh
+++ bench.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+trap "rm radix radix.c data.in data.out" EXIT
+
 futhark dataset --i32-bounds=-9999:9999 -g "[1000000]i32" > data.in
 futhark c radix.fut
 ./radix < data.in > data.out
